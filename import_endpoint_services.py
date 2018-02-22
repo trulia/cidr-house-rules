147a9e556bb9d7d9b49ffe992d050f96c166f03e
Zane Williamson <zanew@zillow.com>
2018-02-22 10:09:27 -0800

    Missing colon

--- import_endpoint_services.py
+++ import_endpoint_services.py
@@ -44,7 +44,7 @@ def import_endpoint_services(event, context):
     try:
         endpoint_services = client.describe_vpc_endpoint_service_configurations()
     except ClientError as e:
-        if e.response['Error']['Code'] = endpoint_service_api_not_available
+        if e.response['Error']['Code'] == endpoint_service_api_not_available:
             logger.info(
                 'VPC Endpoint Service is not available in {}').format(region)
             logger.error('Error: {}'.format(e))
