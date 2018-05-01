467f525e89cb84f07f7a4d62616259fa60a1ca66
zane <zane.williamson@gmail.com>
2018-04-30 20:02:41 -0700

    remove duplicate public ip entries

--- import_nat_gateways.py
+++ import_nat_gateways.py
@@ -1,5 +1,7 @@
 import json
 import os
+import sys
+sys.path.insert(0, './vendor')
 import boto3
 import logging
 import time
@@ -9,10 +11,31 @@ from boto3.dynamodb.conditions import Key, Attr
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+def locate_and_remove_duplicate_nat_id_entry(
+    public_ip, nat_id, nat_table, nat_table_scan):
+    """Function to test for a duplicate entry. This can happen if NAT gateway
+    is created and deleted and the same public IP is reassigned to the account.
+    this will discover and remove previous entry, by passing the TTL process.
+    """
+    for nat in nat_table_scan:
+        if nat['PublicIp'] == public_ip and nat['id'] != nat_id:
+            logger.info(
+                f'Found previously recorded public IP:{public_ip}'
+            )
+            logger.info(f'Removing previous entry: {nat_id}')
+            print(nat_id)
+            print(public_ip)
+            response = nat_table.delete_item(
+                Key={
+                    'id': nat_id
+                }
+            )
+
 def import_nat_gateways(event, context):
     dynamodb = boto3.resource('dynamodb')
     client = boto3.client('ec2')
     nat_table = dynamodb.Table(os.environ['DYNAMODB_TABLE_NAT_GATEWAYS'])
+    nat_table_scan = nat_table.scan()['Items']
     account = event['account']
     region  = event['region']
     # ttl time to expire items in DynamoDB table, default 48 hours
@@ -36,6 +59,10 @@ def import_nat_gateways(event, context):
 
         logger.info('Logging NAT Gateway: {0} for account {1}'.format(
             public_ip, account)
+        )
+
+        locate_and_remove_duplicate_nat_id_entry(
+            public_ip, nat_id, nat_table, nat_table_scan
         )
 
         response = nat_table.put_item(
